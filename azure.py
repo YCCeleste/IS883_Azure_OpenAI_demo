cffa80f99e3267d29a2bc1453f07d8f7f414269d
YCCeleste <122059423+YCCeleste@users.noreply.github.com>
2023-10-29 00:12:08 -0400

    add voice function

--- azure.py
+++ azure.py
@@ -1,6 +1,9 @@
 # Import necessary libraries
 import streamlit as st
 import openai
+import speech_recognition as sr
+import os
+
 
 # Set up your OpenAI API key
 import os
@@ -11,8 +14,30 @@ openai.api_key = os.environ.get('OPENAI_API_KEY')
 # Initialize Streamlit
 st.title("This is Celeste's amazing Chatbot")
 
-# Create a text input field for user queries
-user_input = st.text_input("Ask a question:")
+def transcribe_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        st.write("Please speak something...")
+        audio_data = r.record(source, duration=5)
+        st.write("Recording time is over, wait for a few seconds...")
+    try:
+        text = r.recognize_google(audio_data)
+        return text
+    except sr.UnknownValueError:
+        st.write("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        st.write("Could not request results from Google Speech Recognition service; {0}".format(e))
+    return ""
+
+if st.button("Speak"):
+    user_input = transcribe_audio()
+    if user_input:
+        st.write("You said: " + user_input)
+    else:
+        st.write("Sorry, I could not understand what you said. Please try again.")
+else:
+    user_input = st.text_input("Or type your mood:")
+
 
 # Send the user's query to OpenAI GPT-3
 if user_input:
